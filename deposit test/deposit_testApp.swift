0d3f3d9b13958205f091e2cc3111c14f7e5da021
christopherkarani <chrisbkarani@gmail.com>
2025-02-14 16:01:38 +0300

    Initial Commit

---
+++ deposit test/deposit_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  deposit_testApp.swift
+//  deposit test
+//
+//  Created by Chris Karani on 14/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct deposit_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ deposit testTests/deposit_testTests.swift
@@ -0,0 +1,17 @@
+//
+//  deposit_testTests.swift
+//  deposit testTests
+//
+//  Created by Chris Karani on 14/02/2025.
+//
+
+import Testing
+@testable import deposit_test
+
+struct deposit_testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
